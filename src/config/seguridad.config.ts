51682a26a54f22c7e79f12fa2bcb84af78a0cecb
NaTaLiA7674 <130678658+NaTaLiA7674@users.noreply.github.com>
2023-10-14 13:12:47 -0500

    Modificación de estrategia para poder integrar al microservicio de lógica de negocios

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,6 +1,6 @@
 export namespace ConfiguracionSeguridad {
-  export const claveJWT: string = "";
-  export const menuUsuarioId = "";
+  export const claveJWT: string = "UrbanNav@123*";
+  export const menuUsuarioId = "65248500b7b4ea4d182c8fc0";
   export const listarAccion = "listar";
   export const guardarAccion = "guardar";
   export const editarAccion = "editar";
--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,7 @@ export * from './credenciales.model';
 export * from './factor-de-autenticacion-por-codigo.model';
 export * from './login.model';
 export * from './menu.model';
+export * from './permisos-rol-menu.model';
 export * from './rol-menu.model';
 export * from './rol.model';
 export * from './usuario.model';
